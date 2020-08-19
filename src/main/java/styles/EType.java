60a69464b8a2cf0c2c6a65e2eef8fca71374b5f7
= <=>
2020-08-19 19:12:27 +0300

    Separated font from styles class

---
+++ src/main/java/styles/EType.java
@@ -0,0 +1,39 @@
+package styles;
+
+public enum EType {
+    /**
+     * - Default Cell Style have the following properties:
+     * - Black Texts
+     * - White Background in both headers and texts
+     */
+    TITLE,
+
+    /**
+     * - Default Header Cell Style have the following properties:
+     * - Black Texts
+     * - White Background in both headers and texts
+     */
+    HEADER,
+
+    CELL,
+
+    /**
+     * - Default Formula Cell Style 1 have the following properties:
+     * - Black Texts
+     * - White Background in both headers and texts
+     */
+    FORMULA_1,
+
+    /**
+     * - Default Formula Cell Style 2 have the following properties:
+     * - Black Texts
+     * - White Background in both headers and texts
+     */
+    FORMULA_2,
+
+    DASHED_LIGHT_GREY,
+
+    SOLID_LIGHT_TORQUES
+
+    ;
+}
